68135bc85a25857ee937eed34504966e916b56c6
pguma <p.guma.info+dev@gmail.com>
2022-07-25 00:52:55 +0900

    HttpSession操作ハンズオンの追加

---
+++ src/main/kotlin/lab/pguma/springboothandson/session/HandsOnSessionController.kt
@@ -0,0 +1,30 @@
+package lab.pguma.springboothandson.session
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.servlet.http.HttpSession
+
+@RestController
+class HandsOnSessionController constructor(
+    @Autowired private val session: HttpSession
+) {
+
+    @GetMapping("/call")
+    fun visit(): String {
+        val counter = Optional.ofNullable(session.getAttribute("count"))
+            .map { count -> count as Int + 1 }
+            .orElse(1)
+
+        session.setAttribute("count", counter)
+        return "あなたのコール回数は${counter}回です"
+    }
+
+    @GetMapping("reset")
+    fun reset(): String {
+        session.invalidate()
+        return "コール回数をクリアしました"
+    }
+
+}
